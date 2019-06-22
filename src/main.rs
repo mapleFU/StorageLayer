89c6163aaf431b80391d8b926052666920d3faf1
mapleFU <1506118561@qq.com>
2019-06-22 16:57:34 +0800

    [ADD] add basic put and get api for object storage

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,16 @@ use std::sync::mpsc;
 use std::str::{self, from_utf8};
 use std::io::{self, Cursor, Write};
 use std::fs::{self, File};
-
+use std::path::Path;
 
 use zookeeper::{Acl, CreateMode, Watcher, WatchedEvent, ZooKeeper};
 use zookeeper::recipes::cache::PathChildrenCache;
 use protobuf::{self, Message, ProtobufResult};
+
 use sysinfo::{NetworkExt, System, SystemExt, DiskExt};
 use eventual::Timer;
+
+
 use rocket::{self, get, routes, put};
 use multipart::server::Multipart;
 use multipart::server::save::Entries;
@@ -23,6 +26,7 @@ use rocket::Data;
 use rocket::http::{ContentType, Status};
 use rocket::response::Stream;
 use rocket::response::status::Custom;
+use rocket::config::{Config as RocketConfig, Environment as RocketEnvironment};
 use rocket_multipart_form_data::{mime, MultipartFormDataOptions, MultipartFormData,
                                  MultipartFormDataField, FileField, TextField, RawField};
 
@@ -42,7 +46,7 @@ fn zk_server_urls() -> String {
     let key = "ZOOKEEPER_SERVERS";
     match env::var(key) {
         Ok(val) => val,
-        Err(_) => "localhost:2181".to_string(),
+        Err(_) => "maplewish.cn:2181".to_string(),
     }
 }
 
@@ -50,15 +54,24 @@ fn zk_server_urls() -> String {
 const ZKDIR: &'static str = "/fs";
 const DATA_DIR: &'static str = "data";
 
+#[put("/data/<uid>", data = "<data>")]
+fn handle_post(uid: String, data: Data)->Result<(), Custom<String>> {
+    println!("Request {}", uid);
+//    println!("Got a put request raw data");
+    data.stream_to_file(format!("data/{}", uid)).map(|n| n.to_string());
+    Ok(())
+}
+
+
 #[put("/", data = "<data>")]
 fn handle_multi(cont_type: &ContentType, data: Data) -> Result<Stream<Cursor<Vec<u8>>>, Custom<String>>{
     // this and the next check can be implemented as a request guard but it seems like just
     // more boilerplate than necessary
+    println!("Something begin in this request");
     if !cont_type.is_form_data() {
-        return Err(Custom(
-            Status::BadRequest,
-            "Content-Type not multipart/form-data".into()
-        ));
+        println!("Got a put request raw data");
+        data.stream_to_file("data/demo").map(|n| n.to_string());
+        return Ok(Stream::from(Cursor::new(Vec::from("nmsl"))));
     }
 
     let (_, boundary) = cont_type.params().find(|&(k, _)| k == "boundary").ok_or_else(
@@ -76,6 +89,43 @@ fn handle_multi(cont_type: &ContentType, data: Data) -> Result<Stream<Cursor<Vec
     Ok(Stream::from(Cursor::new(Vec::from("nmsl"))))
 }
 
+#[get("/data/<uid>")]
+fn handle_get(uid: String)->Result<rocket::response::NamedFile, Custom<String>> {
+    println!("Get uid {}", uid);
+
+    let path = String::from("data/") + &uid;
+    let file_path = Path::new(&path);
+
+    if !file_path.exists() && file_path.is_file() {
+        return Err(Custom(Status::NotFound, "NotFound".to_string()))
+    }
+    // TODO: unuse unwrap
+    Ok(rocket::response::NamedFile::open(file_path).unwrap())
+}
+
+
+
+fn zn_conn() -> Server {
+    // connection to zookeeper
+    let mut zk_pb = zk::Server::new();
+    zk_pb.host = match env::var("ZMQ_HOST") {
+        Ok(val) => val,
+        Err(_) => "localhost".to_string(),
+    };
+
+    zk_pb.zmq_tcp_port = match env::var("ZMQ_HOST") {
+        Ok(val) => val,
+        Err(_) => "11234".to_string(),
+    };
+
+    zk_pb.http_port = match env::var("HTTP_PORT") {
+        Ok(val) => val,
+        Err(_) => "8000".to_string()
+    };
+
+    zk_pb
+}
+
 
 fn main() {
 
@@ -89,28 +139,61 @@ fn main() {
 
     let ctx = zmq::Context::new();
 
-    let mut zmq_socket = ctx.socket(zmq::STREAM).unwrap();
-    zmq_socket.connect("tcp://localhost:11234").unwrap();
+    let mut zmq_socket = ctx.socket(zmq::SUB).unwrap();
+    let mut zmq_push_socket = ctx.socket(zmq::PUSH).unwrap();
+
+    thread::spawn(move || {
+        if let Err(e) = zmq_socket.connect("tcp://127.0.0.1:11234") {
+            println!("{:?}", e);
+            panic!();
+        }
+        if let Err(e) = zmq_push_socket.connect("tcp://127.0.0.1:5558") {
+            println!("{:?}", e);
+            panic!();
+        }
+        zmq_socket.set_subscribe("object ".as_bytes());
+        loop {
+            let mut message = zmq::Message::new().unwrap();
+            let rs = zmq_socket.recv(&mut message, zmq::DONTWAIT);
+
+
+            if let Err(r) = rs {
+                if r != zmq::Error::EAGAIN {
+                    println!("{:?}", r);
+                }
+                thread::sleep(Duration::from_millis(1));
+                continue;
+            }
+            match message.as_str() {
+                None => println!("Recv None"),
+                Some(s) => {
+                    let cur_path = String::from("data/") + s;
+                    if Path::new(&cur_path).exists() {
+                        println!("Receive request for path {}", cur_path);
+                        let zn_msg = zn_conn();
+                        let zn_msg: Vec<u8> =  Vec::from(zn_msg.write_to_bytes().unwrap());
+                        zmq_push_socket.send(zn_msg.as_slice(), zmq::DONTWAIT);
+                    }
+                }
+            };
+        }
+    });
 
     // connection to zookeeper
-    let mut zk_conn = zk::Server::new();
-    zk_conn.zmq_host = match env::var("ZMQ_HOST") {
-        Ok(val) => val,
-        Err(_) => "localhost".to_string(),
-    };
+    let mut zk_pb = zn_conn();
 
-    zk_conn.zmq_tcp_port = match env::var("ZMQ_HOST") {
-        Ok(val) => val,
-        Err(_) => "11234".to_string(),
-    };
+    let http_port_i32 = zk_pb.http_port.clone().parse::<u16>().unwrap();
 
 //    protobuf::parse_from_bytes()
-    let mut server_data: Vec<u8> = Vec::from(zk_conn.write_to_bytes().unwrap());
+    let mut server_data: Vec<u8> = Vec::from(zk_pb.write_to_bytes().unwrap());
 
     // call once
     if let Err(e) = zk.create(ZKDIR, Vec::new(), Acl::open_unsafe().clone(), CreateMode::Persistent) {
         println!("{:?}", e);
+    } else {
+        println!("Created");
     }
+
     // create current node
     // TODO: set acl for this
     let node = zk.create("/fs/Node",
@@ -128,19 +211,29 @@ fn main() {
             let (mut total_space, mut available_space) = (0, 0);
             for disk in sys.get_disks() {
                 // TODO: make clear if it's ok
-//                println!("{} {}", disk.get_total_space(), disk.get_available_space());
+//                println!("{} {} mount on {:?} ", disk.get_total_space(), disk.get_available_space(), disk.get_mount_point());
                 if disk.get_total_space() > total_space {
                     total_space = disk.get_total_space();
                     available_space = disk.get_available_space();
                 }
             }
-            zk_conn.available_space = available_space;
-            zk_conn.total_space = total_space;
+            zk_pb.available_space = available_space;
+            zk_pb.total_space = total_space;
             // send to zk
-            let mut server_data: Vec<u8> = Vec::from(zk_conn.write_to_bytes().unwrap());
+            let mut server_data: Vec<u8> = Vec::from(zk_pb.write_to_bytes().unwrap());
             zk.set_data(&node, server_data, None);
         }
     });
 
-    rocket::ignite().mount("/", routes![handle_multi]).launch();
+
+    let config = RocketConfig::build(RocketEnvironment::Staging)
+        .address("0.0.0.0")
+        .port(http_port_i32)
+        .finalize().unwrap();
+
+    rocket::custom(config)
+        .mount("/", routes![handle_multi])
+        .mount("/",  routes![handle_post])
+        .mount("/", routes![handle_get])
+        .launch();
 }
--- src/zk.rs
+++ src/zk.rs
@@ -25,9 +25,10 @@ use protobuf::ProtobufEnum as ProtobufEnum_imported_for_functions;
 pub struct Server {
     // message fields
     pub zmq_tcp_port: ::std::string::String,
-    pub zmq_host: ::std::string::String,
+    pub host: ::std::string::String,
     pub total_space: u64,
     pub available_space: u64,
+    pub http_port: ::std::string::String,
     // special fields
     pub unknown_fields: ::protobuf::UnknownFields,
     pub cached_size: ::protobuf::CachedSize,
@@ -70,30 +71,30 @@ impl Server {
         ::std::mem::replace(&mut self.zmq_tcp_port, ::std::string::String::new())
     }
 
-    // string zmq_host = 2;
+    // string host = 2;
 
 
-    pub fn get_zmq_host(&self) -> &str {
-        &self.zmq_host
+    pub fn get_host(&self) -> &str {
+        &self.host
     }
-    pub fn clear_zmq_host(&mut self) {
-        self.zmq_host.clear();
+    pub fn clear_host(&mut self) {
+        self.host.clear();
     }
 
     // Param is passed by value, moved
-    pub fn set_zmq_host(&mut self, v: ::std::string::String) {
-        self.zmq_host = v;
+    pub fn set_host(&mut self, v: ::std::string::String) {
+        self.host = v;
     }
 
     // Mutable pointer to the field.
     // If field is not initialized, it is initialized with default value first.
-    pub fn mut_zmq_host(&mut self) -> &mut ::std::string::String {
-        &mut self.zmq_host
+    pub fn mut_host(&mut self) -> &mut ::std::string::String {
+        &mut self.host
     }
 
     // Take field
-    pub fn take_zmq_host(&mut self) -> ::std::string::String {
-        ::std::mem::replace(&mut self.zmq_host, ::std::string::String::new())
+    pub fn take_host(&mut self) -> ::std::string::String {
+        ::std::mem::replace(&mut self.host, ::std::string::String::new())
     }
 
     // uint64 total_space = 3;
@@ -125,6 +126,32 @@ impl Server {
     pub fn set_available_space(&mut self, v: u64) {
         self.available_space = v;
     }
+
+    // string http_port = 5;
+
+
+    pub fn get_http_port(&self) -> &str {
+        &self.http_port
+    }
+    pub fn clear_http_port(&mut self) {
+        self.http_port.clear();
+    }
+
+    // Param is passed by value, moved
+    pub fn set_http_port(&mut self, v: ::std::string::String) {
+        self.http_port = v;
+    }
+
+    // Mutable pointer to the field.
+    // If field is not initialized, it is initialized with default value first.
+    pub fn mut_http_port(&mut self) -> &mut ::std::string::String {
+        &mut self.http_port
+    }
+
+    // Take field
+    pub fn take_http_port(&mut self) -> ::std::string::String {
+        ::std::mem::replace(&mut self.http_port, ::std::string::String::new())
+    }
 }
 
 impl ::protobuf::Message for Server {
@@ -140,7 +167,7 @@ impl ::protobuf::Message for Server {
                     ::protobuf::rt::read_singular_proto3_string_into(wire_type, is, &mut self.zmq_tcp_port)?;
                 },
                 2 => {
-                    ::protobuf::rt::read_singular_proto3_string_into(wire_type, is, &mut self.zmq_host)?;
+                    ::protobuf::rt::read_singular_proto3_string_into(wire_type, is, &mut self.host)?;
                 },
                 3 => {
                     if wire_type != ::protobuf::wire_format::WireTypeVarint {
@@ -155,6 +182,9 @@ impl ::protobuf::Message for Server {
                     }
                     let tmp = is.read_uint64()?;
                     self.available_space = tmp;
+                },
+                5 => {
+                    ::protobuf::rt::read_singular_proto3_string_into(wire_type, is, &mut self.http_port)?;
                 },
                 _ => {
                     ::protobuf::rt::read_unknown_or_skip_group(field_number, wire_type, is, self.mut_unknown_fields())?;
@@ -171,14 +201,17 @@ impl ::protobuf::Message for Server {
         if !self.zmq_tcp_port.is_empty() {
             my_size += ::protobuf::rt::string_size(1, &self.zmq_tcp_port);
         }
-        if !self.zmq_host.is_empty() {
-            my_size += ::protobuf::rt::string_size(2, &self.zmq_host);
+        if !self.host.is_empty() {
+            my_size += ::protobuf::rt::string_size(2, &self.host);
         }
         if self.total_space != 0 {
             my_size += ::protobuf::rt::value_size(3, self.total_space, ::protobuf::wire_format::WireTypeVarint);
         }
         if self.available_space != 0 {
             my_size += ::protobuf::rt::value_size(4, self.available_space, ::protobuf::wire_format::WireTypeVarint);
+        }
+        if !self.http_port.is_empty() {
+            my_size += ::protobuf::rt::string_size(5, &self.http_port);
         }
         my_size += ::protobuf::rt::unknown_fields_size(self.get_unknown_fields());
         self.cached_size.set(my_size);
@@ -189,14 +222,17 @@ impl ::protobuf::Message for Server {
         if !self.zmq_tcp_port.is_empty() {
             os.write_string(1, &self.zmq_tcp_port)?;
         }
-        if !self.zmq_host.is_empty() {
-            os.write_string(2, &self.zmq_host)?;
+        if !self.host.is_empty() {
+            os.write_string(2, &self.host)?;
         }
         if self.total_space != 0 {
             os.write_uint64(3, self.total_space)?;
         }
         if self.available_space != 0 {
             os.write_uint64(4, self.available_space)?;
+        }
+        if !self.http_port.is_empty() {
+            os.write_string(5, &self.http_port)?;
         }
         os.write_unknown_fields(self.get_unknown_fields())?;
         ::std::result::Result::Ok(())
@@ -246,9 +282,9 @@ impl ::protobuf::Message for Server {
                     |m: &mut Server| { &mut m.zmq_tcp_port },
                 ));
                 fields.push(::protobuf::reflect::accessor::make_simple_field_accessor::<_, ::protobuf::types::ProtobufTypeString>(
-                    "zmq_host",
-                    |m: &Server| { &m.zmq_host },
-                    |m: &mut Server| { &mut m.zmq_host },
+                    "host",
+                    |m: &Server| { &m.host },
+                    |m: &mut Server| { &mut m.host },
                 ));
                 fields.push(::protobuf::reflect::accessor::make_simple_field_accessor::<_, ::protobuf::types::ProtobufTypeUint64>(
                     "total_space",
@@ -259,6 +295,11 @@ impl ::protobuf::Message for Server {
                     "available_space",
                     |m: &Server| { &m.available_space },
                     |m: &mut Server| { &mut m.available_space },
+                ));
+                fields.push(::protobuf::reflect::accessor::make_simple_field_accessor::<_, ::protobuf::types::ProtobufTypeString>(
+                    "http_port",
+                    |m: &Server| { &m.http_port },
+                    |m: &mut Server| { &mut m.http_port },
                 ));
                 ::protobuf::reflect::MessageDescriptor::new::<Server>(
                     "Server",
@@ -283,9 +324,10 @@ impl ::protobuf::Message for Server {
 impl ::protobuf::Clear for Server {
     fn clear(&mut self) {
         self.zmq_tcp_port.clear();
-        self.zmq_host.clear();
+        self.host.clear();
         self.total_space = 0;
         self.available_space = 0;
+        self.http_port.clear();
         self.unknown_fields.clear();
     }
 }
@@ -303,27 +345,32 @@ impl ::protobuf::reflect::ProtobufValue for Server {
 }
 
 static file_descriptor_proto_data: &'static [u8] = b"\
-    \n\x08zk.proto\"\x8f\x01\n\x06Server\x12\x20\n\x0czmq_tcp_port\x18\x01\
-    \x20\x01(\tR\nzmqTcpPort\x12\x19\n\x08zmq_host\x18\x02\x20\x01(\tR\x07zm\
-    qHost\x12\x1f\n\x0btotal_space\x18\x03\x20\x01(\x04R\ntotalSpace\x12'\n\
-    \x0favailable_space\x18\x04\x20\x01(\x04R\x0eavailableSpaceJ\xe2\x02\n\
-    \x06\x12\x04\0\0\n\x01\n\x08\n\x01\x0c\x12\x03\0\0\x12\n\n\n\x02\x04\0\
-    \x12\x04\x02\0\n\x01\n\n\n\x03\x04\0\x01\x12\x03\x02\x08\x0e\n\x1b\n\x04\
-    \x04\0\x02\0\x12\x03\x04\x04\x1c\x1a\x0e\x20addr\x20for\x20zmq\n\n\r\n\
-    \x05\x04\0\x02\0\x04\x12\x04\x04\x04\x02\x10\n\x0c\n\x05\x04\0\x02\0\x05\
-    \x12\x03\x04\x04\n\n\x0c\n\x05\x04\0\x02\0\x01\x12\x03\x04\x0b\x17\n\x0c\
-    \n\x05\x04\0\x02\0\x03\x12\x03\x04\x1a\x1b\n\x1b\n\x04\x04\0\x02\x01\x12\
-    \x03\x06\x04\x18\x1a\x0e\x20host\x20for\x20zmq\n\n\r\n\x05\x04\0\x02\x01\
-    \x04\x12\x04\x06\x04\x04\x1c\n\x0c\n\x05\x04\0\x02\x01\x05\x12\x03\x06\
-    \x04\n\n\x0c\n\x05\x04\0\x02\x01\x01\x12\x03\x06\x0b\x13\n\x0c\n\x05\x04\
-    \0\x02\x01\x03\x12\x03\x06\x16\x17\n\x0b\n\x04\x04\0\x02\x02\x12\x03\x08\
-    \x04\x1b\n\r\n\x05\x04\0\x02\x02\x04\x12\x04\x08\x04\x06\x18\n\x0c\n\x05\
-    \x04\0\x02\x02\x05\x12\x03\x08\x04\n\n\x0c\n\x05\x04\0\x02\x02\x01\x12\
-    \x03\x08\x0b\x16\n\x0c\n\x05\x04\0\x02\x02\x03\x12\x03\x08\x19\x1a\n\x0b\
-    \n\x04\x04\0\x02\x03\x12\x03\t\x04\x1f\n\r\n\x05\x04\0\x02\x03\x04\x12\
-    \x04\t\x04\x08\x1b\n\x0c\n\x05\x04\0\x02\x03\x05\x12\x03\t\x04\n\n\x0c\n\
-    \x05\x04\0\x02\x03\x01\x12\x03\t\x0b\x1a\n\x0c\n\x05\x04\0\x02\x03\x03\
-    \x12\x03\t\x1d\x1eb\x06proto3\
+    \n\x08zk.proto\"\xa5\x01\n\x06Server\x12\x20\n\x0czmq_tcp_port\x18\x01\
+    \x20\x01(\tR\nzmqTcpPort\x12\x12\n\x04host\x18\x02\x20\x01(\tR\x04host\
+    \x12\x1f\n\x0btotal_space\x18\x03\x20\x01(\x04R\ntotalSpace\x12'\n\x0fav\
+    ailable_space\x18\x04\x20\x01(\x04R\x0eavailableSpace\x12\x1b\n\thttp_po\
+    rt\x18\x05\x20\x01(\tR\x08httpPortJ\xe3\x03\n\x06\x12\x04\0\0\x0f\x01\n\
+    \x08\n\x01\x0c\x12\x03\0\0\x12\n\n\n\x02\x04\0\x12\x04\x02\0\x0f\x01\n\n\
+    \n\x03\x04\0\x01\x12\x03\x02\x08\x0e\n\x1b\n\x04\x04\0\x02\0\x12\x03\x04\
+    \x04\x1c\x1a\x0e\x20addr\x20for\x20zmq\n\n\r\n\x05\x04\0\x02\0\x04\x12\
+    \x04\x04\x04\x02\x10\n\x0c\n\x05\x04\0\x02\0\x05\x12\x03\x04\x04\n\n\x0c\
+    \n\x05\x04\0\x02\0\x01\x12\x03\x04\x0b\x17\n\x0c\n\x05\x04\0\x02\0\x03\
+    \x12\x03\x04\x1a\x1b\n\x1b\n\x04\x04\0\x02\x01\x12\x03\x06\x04\x14\x1a\
+    \x0e\x20host\x20for\x20zmq\n\n\r\n\x05\x04\0\x02\x01\x04\x12\x04\x06\x04\
+    \x04\x1c\n\x0c\n\x05\x04\0\x02\x01\x05\x12\x03\x06\x04\n\n\x0c\n\x05\x04\
+    \0\x02\x01\x01\x12\x03\x06\x0b\x0f\n\x0c\n\x05\x04\0\x02\x01\x03\x12\x03\
+    \x06\x12\x13\n\"\n\x04\x04\0\x02\x02\x12\x03\t\x04\x1b\x1a\x15\x20data\
+    \x20for\x20disk\x20space\n\n\r\n\x05\x04\0\x02\x02\x04\x12\x04\t\x04\x06\
+    \x14\n\x0c\n\x05\x04\0\x02\x02\x05\x12\x03\t\x04\n\n\x0c\n\x05\x04\0\x02\
+    \x02\x01\x12\x03\t\x0b\x16\n\x0c\n\x05\x04\0\x02\x02\x03\x12\x03\t\x19\
+    \x1a\n\x0b\n\x04\x04\0\x02\x03\x12\x03\n\x04\x1f\n\r\n\x05\x04\0\x02\x03\
+    \x04\x12\x04\n\x04\t\x1b\n\x0c\n\x05\x04\0\x02\x03\x05\x12\x03\n\x04\n\n\
+    \x0c\n\x05\x04\0\x02\x03\x01\x12\x03\n\x0b\x1a\n\x0c\n\x05\x04\0\x02\x03\
+    \x03\x12\x03\n\x1d\x1e\n/\n\x04\x04\0\x02\x04\x12\x03\r\x04\x19\x1a\"\
+    \x20host\x20to\x20interact\x20with\x20http\x20file\x20\n\n\r\n\x05\x04\0\
+    \x02\x04\x04\x12\x04\r\x04\n\x1f\n\x0c\n\x05\x04\0\x02\x04\x05\x12\x03\r\
+    \x04\n\n\x0c\n\x05\x04\0\x02\x04\x01\x12\x03\r\x0b\x14\n\x0c\n\x05\x04\0\
+    \x02\x04\x03\x12\x03\r\x17\x18b\x06proto3\
 ";
 
 static mut file_descriptor_proto_lazy: ::protobuf::lazy::Lazy<::protobuf::descriptor::FileDescriptorProto> = ::protobuf::lazy::Lazy {
